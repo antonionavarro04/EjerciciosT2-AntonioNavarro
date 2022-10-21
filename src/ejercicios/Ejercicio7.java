021d95b9e08e3c670a8026e48b34cd973baee2d6
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-21 09:22:48 +0200

    66 a 62

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -42,14 +42,10 @@ public class Ejercicio7 {
             if (segundos == 60) {
                 segundos = 0;
                 minutos++;
-            }
-
-            if (minutos == 60) {
+            } if (minutos == 60) {
                 minutos = 0;
                 horas++;
-            }
-
-            if (horas == 24) {
+            } if (horas == 24) {
                 horas = 0;
             }
         }
